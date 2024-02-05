fe19208f14c2943e49ff598fa63f1bf89de896e2
Saniya123 <saniyarsacare@gmail.com>
2024-02-05 12:41:31 +0530

    updated

--- src/components/PostJob/PostJobMain/PostJobMain.jsx
+++ src/components/PostJob/PostJobMain/PostJobMain.jsx
@@ -7,7 +7,7 @@ const PostJobMain = () => {
             <div className='py-24'>
                 <div className='w-full flex flex-col items-center justify-center leading-10 gap-2'>
                     <p className='text-sm text-btnBlue font-bold tracking-wider'>LET’S FIND OUT</p>
-                    <h1 className='text-black font-black text-3xl tracking-wider'>How It Works</h1>
+                    <h1 className='text-black font-black text-5xl tracking-wider'>How It Works</h1>
                     <h3 className='text-grayShade font-bold tracking-wider'>
                         Lorem ipsum dolor sit amet, cibo mundi ea duo, vim exerci phaedrum
                     </h3>
@@ -21,7 +21,7 @@ const PostJobMain = () => {
             </div>
 
             <PostJobForm />
-            
+
         </div>
     )
 }
